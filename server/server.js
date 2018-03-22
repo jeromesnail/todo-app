506f15ff8ffcbe3b63da3b74c4b67a67ae38d1e3
jeromesnail <jeromesnail@hotmail.fr>
2018-03-22 19:04:33 +0100

    AddedGET /users/me ans auth middleware

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ const { ObjectID } = require('mongodb');
 const { mongoose } = require('./db/mongoose');
 const { Todo } = require('./models/todo');
 const { User } = require('./models/user');
+const { authenticate } = require('./middleware/authenticate');
 
 const port = process.env.PORT;
 
@@ -126,6 +127,10 @@ app.post('/users', (req, res) => {
         description: e
       });
     });
+});
+
+app.get('/users/me', authenticate, (req, res) => {
+  res.send({ user: req.user });
 });
 
 app.listen(port, () => {
