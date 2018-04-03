d41193a59032b985a90b42c29635025593752e64
jeromesnail <jeromesnail@hotmail.fr>
2018-04-03 11:03:13 +0200

    Updated users route with async/await

--- server/routes/users.js
+++ server/routes/users.js
@@ -4,58 +4,52 @@ module.exports = (app) => {
   const { User } = require('../models/user');
   const { authenticate } = require('../middleware/authenticate');
 
-  app.post('/users', (req, res) => {
-    const body = _.pick(req.body, ['email', 'password']);
-    const user = new User(body);
-    user.save()
-      .then(() => {
-        return user.generateAuthToken();
-      })
-      .then(token => {
-        res.header('x-auth', token).send({ status: 200, user });
-      })
-      .catch(e => {
-        res.status(400).send({
-          status: 400,
-          error: 'Failed to add the user'
-        });
+  app.post('/users', async (req, res) => {
+    try {
+      const body = _.pick(req.body, ['email', 'password']);
+      const user = new User(body);
+      await user.save();
+      const token = await user.generateAuthToken();
+      res.header('x-auth', token).send({ status: 200, user });
+    } catch (e) {
+      res.status(400).send({
+        status: 400,
+        error: 'Failed to add the user'
       });
+    }
   });
 
   app.get('/users/me', authenticate, (req, res) => {
     res.send({ status: 200, user: req.user });
   });
 
-  app.post('/users/login', (req, res) => {
-    const { email, password } = _.pick(req.body, ['email', 'password']);
-    User.findByCredentials(email, password)
-      .then(user => {
-        return user.generateAuthToken()
-          .then(token => {
-            res.header('x-auth', token).send({ status: 200, user });
-          });
-      })
-      .catch(e => {
-        res.status(400).send({
-          status: 400,
-          error: 'Failed to anthentificate'
-        });
+  app.post('/users/login', async (req, res) => {
+    try {
+      const { email, password } = _.pick(req.body, ['email', 'password']);
+      const user = await User.findByCredentials(email, password);
+      const token = await user.generateAuthToken();
+      res.header('x-auth', token).send({ status: 200, user });
+    } catch (e) {
+      res.status(400).send({
+        status: 400,
+        error: 'Failed to anthentificate'
       });
+    }
   });
 
-  app.delete('/users/me/token', authenticate, (req, res) => {
-    req.user.removeToken(req.token)
-      .then(() => {
-        res.status(200).send({
-          status: 200,
-          message: 'User correctly logged out'
-        });
-      }).catch(e => {
-        res.status(400).send({
-          status: 400,
-          error: 'Failed to log out'
-        });
+  app.delete('/users/me/token', authenticate, async (req, res) => {
+    try {
+      await req.user.removeToken(req.token);
+      res.status(200).send({
+        status: 200,
+        message: 'User correctly logged out'
       });
+    } catch (e) {
+      res.status(400).send({
+        status: 400,
+        error: 'Failed to log out'
+      });
+    }
   });
 
 };
